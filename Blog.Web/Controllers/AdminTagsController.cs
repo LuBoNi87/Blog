0794883198e76707ad68e621b5245d18aedb95da
Luput Bogdan <luputbogdan87@yahoo.com>
2023-05-24 17:55:20 +0300

    Saving Tags to DataBase

--- Blog.Web/Controllers/AdminTagsController.cs
+++ Blog.Web/Controllers/AdminTagsController.cs
@@ -1,13 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.Web.Data;
+using Blog.Web.Models.Domain;
+using Blog.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Web.Controllers
 {
     public class AdminTagsController : Controller
     {
+        private readonly BlogDbContext blogDbContext;
+
+        public AdminTagsController(BlogDbContext blogDbContext)
+        {
+            this.blogDbContext = blogDbContext;
+        }
+
+
         [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            // Mapping AddTagRequest to Tag domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            };
+
+            blogDbContext.Tags.Add(tag);
+            blogDbContext.SaveChanges();
+
+            return View("Add");
         }
     }
 }
